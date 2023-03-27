434b36d78b49795dc5907490aa54f5a1140aaf9e
Rustam Baybakov <rustam220702@mail.ru>
2023-03-27 17:00:34 +0300

    Step 2 - 2

--- service/clients/models.py
+++ service/clients/models.py
@@ -3,3 +3,8 @@ from django.db import models
 
 class Client(models.Model):
     user = models.OneToOneField(User, on_delete=models.PROTECT)
+    company_name = models.CharField(max_length=100)
+    full_adress = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"Client:{self.company_name}"
